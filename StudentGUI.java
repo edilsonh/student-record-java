812274b68be65bf322effe3c54037bc87b605e21
Edilson Hernandez <edilson.hernandez1@gmail.com>
2018-03-03 18:46:14 -0500

    Add header and comments

--- StudentGUI.java
+++ StudentGUI.java
@@ -1,3 +1,9 @@
+/*
+* File: StudentGUI.java
+* Author: Edilson Hernandez
+* Date: March 4, 2018
+* Purpose: The purpose of this program is create a database of student records.
+*/
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -38,23 +44,31 @@ public class StudentGUI extends JFrame {
 
     process.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
+        // start with if statement to check if id entered is a integer
         if (checkInt(idText.getText())) {
+          // store id number
           int num = Integer.parseInt(idText.getText());
+          // get selected option
           String selection = cb.getItemAt(cb.getSelectedIndex());
+          // determine next step with if statement depending on selection
           if (selection == "Insert") {
+            // ensure that user fills all fields and id is available
             if ((nameText.getText().trim().equals("")) || (majorText.getText().trim().equals(""))) {
               JOptionPane.showMessageDialog(new JFrame(), "Please fill in all fields!");
             } else if (hm.get(num) == null) {
+              // store student record in hashmap
               hm.put(num, new Student(nameText.getText(), majorText.getText()));
               String success = "Success! The following has been entered:\nId: " + num +
               "\nName: " + nameText.getText() +
               "\nMajor: " + majorText.getText();
+              // display success message of student record stored
               JOptionPane.showMessageDialog(new JFrame(), success);
             } else {
               JOptionPane.showMessageDialog(new JFrame(), "That Id is already taken!");
             }
           } else if (selection == "Delete") {
             Student stdnt = hm.get(num);
+            // ensure that student record exist
             if (stdnt == null) {
               JOptionPane.showMessageDialog(new JFrame(), "Student not found...");
             } else {
@@ -63,6 +77,7 @@ public class StudentGUI extends JFrame {
             }
           } else if (selection == "Find") {
             Student stdnt = hm.get(num);
+            // ensure that student record exist
             if (stdnt == null) {
               JOptionPane.showMessageDialog(new JFrame(), "Student not found...");
             } else {
@@ -70,6 +85,7 @@ public class StudentGUI extends JFrame {
               JOptionPane.showMessageDialog(new JFrame(), found);
             }
           } else if (selection == "Update") {
+            // ensure that student record exist
             if (hm.get(num) == null) {
               JOptionPane.showMessageDialog(new JFrame(), "Student not found...");
             } else {
@@ -78,6 +94,7 @@ public class StudentGUI extends JFrame {
               Object grades[] = {'A','B','C','D','F'};
               Object c = JOptionPane.showInputDialog(new JFrame(), "How many credits was the class?", null, JOptionPane.QUESTION_MESSAGE, null, credits, credits[0]);
               Object g = JOptionPane.showInputDialog(new JFrame(), "What was the grade received?", null, JOptionPane.QUESTION_MESSAGE, null, grades, grades[0]);
+              // based on entry, add grades appropiately to student record
               switch (g.toString()) {
                 case "A": stdnt.courseCompleted(Integer.parseInt(c.toString()),4);
                           break;
@@ -108,6 +125,7 @@ public class StudentGUI extends JFrame {
     setSize(300,300);
   }
 
+  // method to determine if given string an integer
   public boolean checkInt(String s){
     try {
       Integer.parseInt(s);
